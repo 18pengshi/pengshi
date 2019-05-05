1a6a9718f044e8943acdf33e8b036f29ef98fd7f
18pengshi <49258657+18pengshi@users.noreply.github.com>
2019-05-05 08:55:33 +0800

    Add files via upload

---
+++ yu JU1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body>
+<?php
+$favcolor="red";
+switch ($avcolor)
+{
+case "red":
+   echo "你喜欢的颜色是红色!";
+   break;
+case "blue":
+   echo "你喜欢的颜色是蓝色!";
+   break;
+case "green":
+    echo"你喜欢的颜色是绿色！";
+	break;
+default:
+    echo "你喜欢的颜色不是 红，蓝，或绿色！";
+}
+
+?>
+</body>
+</html>
