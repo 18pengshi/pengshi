60cf5f8444824590fd030fd6d921b7f077a1e39d
18pengshi <49258657+18pengshi@users.noreply.github.com>
2019-05-09 09:54:18 +0800

    Add files via upload

---
+++ tu xiang chu li.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+
+<body> 
+<?php
+array(12) {
+	["gd version"]=>
+	string(26) "bundled (2.1.0 compatible)"
+	["freetype sypport"]=>
+	bool(true)
+	["freetype linkape"]=>
+	string(13)"with freetype"
+	["t1lib support"]=>
+	bool(false)
+	["gif read support"]=>
+	bool(true)
+	["gif create support"]=>
+	bool(true)
+	["jpeg support"]=>
+	bool(true)
+	["png support"]=>
+	bool(true)
+	["wbmp support"]=>
+	bool(true)
+	["xpm support"]=>
+	bool(false)
+	["xbm support"]=>
+	bool(true)
+	["jis-mapped japanese font support"]=>
+	bool(false)
+}
+?>
+</body>
+</html>
